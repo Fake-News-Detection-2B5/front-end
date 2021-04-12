b169a8776442e263c317979422e9cd1e26e0217c
UnexomWid <uw@exom.dev>
2021-04-12 11:04:41 +0300

    [ADD] Navbar username dropdown

--- src/components/common/CommonNavbar.jsx
+++ src/components/common/CommonNavbar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 import '../../style/navbar.scss';
@@ -10,7 +10,7 @@ class CommonNavbar extends Component {
     render() {
       return (
         <React.Fragment>
-            <Navbar bg="white" expand="sm" className="sticky-top border-bottom">
+            <Navbar id="navbar" bg="white" expand="sm" className="sticky-top border-bottom">
                 <LinkContainer to="/">
                     <Navbar.Brand>
                         <img
@@ -19,9 +19,42 @@ class CommonNavbar extends Component {
                             height="32"
                             src={process.env.PUBLIC_URL + "/res/img/logo512.png"}
                             alt="Brand"
-                            />
+                        />
                     </Navbar.Brand>
                 </LinkContainer>
+                {
+                    this.props.authenticated ?
+                        <Navbar.Collapse>
+                            <Nav className="ml-auto">
+                                <NavDropdown title={
+                                    <React.Fragment>
+                                        <img
+                                            className="image-brand"
+                                            width="32"
+                                            height="32"
+                                            src={process.env.PUBLIC_URL + "/res/img/logo512.png"}
+                                            alt="Username"
+                                        />
+                                        <span id="username">
+                                            Username
+                                        </span>
+                                    </React.Fragment>}
+                                >
+                                    <LinkContainer to="/profile">
+                                        <NavDropdown.Item>
+                                            Profile
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to="/settings">
+                                        <NavDropdown.Item>
+                                            Settings
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                </NavDropdown>
+                            </Nav>
+                        </Navbar.Collapse>
+                    : ""
+                }
             </Navbar>
         </React.Fragment>
       );
