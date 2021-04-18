aca5c05630b7d42763b5651bf643d6a71ce9ec76
UnexomWid <uw@exom.dev>
2021-04-18 12:59:51 +0300

    [ADD] Settings page search and pagination capabilities

--- src/components/pages/PageSettings.jsx
+++ src/components/pages/PageSettings.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Button, Tabs, Tab, Form } from "react-bootstrap";
+import { Button, Tabs, Tab, Form, Pagination } from "react-bootstrap";
 
 import CommonNavbar from "../common/CommonNavbar.jsx";
 import CommonFooter from "../common/CommonFooter.jsx";
 
 import "../../style/settings.scss";
 import ProviderPreference from "../utility/ProviderPreference.jsx";
+
+const PAGINATION_VISIBLE_COUNT = 3;
 
 class PageSettings extends Component {
   state = {
@@ -18,8 +20,39 @@ class PageSettings extends Component {
       name: "Digi24",
       credibility: "99",
       avatar: "http://www.digi24.ro/static/theme-repo/bin/images/digi24-logo.png"
-    }]
+    }],
+    searchQuery: "",
+    pagination: {
+      index: 1,
+      count: 10
+    }
   };
+
+  handlePaginationFactory = (i) => {
+    return (event) => this.setState({
+      pagination: {
+        ...this.state.pagination,
+        index: i
+      }
+    });
+  }
+
+  handleSearchKeyPress = (event) => {
+    if(event.key === "Enter") {
+      this.handleSearch();
+      event.preventDefault();
+    }
+  }
+
+  handleSearchChange = (event) => {
+    this.setState({ searchQuery: event.target.value });
+    console.log(this.state.searchQuery);
+  }
+
+  handleSearch = () => {
+    // request.
+    console.log(`Search request: '${this.state.searchQuery}'`);
+  }
 
   render() {
     return (
@@ -81,9 +114,63 @@ class PageSettings extends Component {
             <Tab eventKey="preferences" title="Preferences Settings">
               <div className="tab-custom-container">
                 <div id="preferences-settings-container">
+                  <Form id="preferences-settings-search">
+                    <Form.Control type="text" placeholder="Search..." onChange={this.handleSearchChange} onKeyPress={this.handleSearchKeyPress} />
+                    <Button id="preferences-settings-search-button" variant="primary" onClick={this.handleSearch}>
+                      Search
+                    </Button>
+                  </Form>
                   {this.state.providers.map(provider => {
                     return <ProviderPreference {...provider} />
                   })}
+                  <Pagination>
+                    <Pagination.First onClick={this.handlePaginationFactory(1)} />
+                    <Pagination.Prev onClick={this.handlePaginationFactory(this.state.pagination.index - 1)} />
+
+                    {
+                      this.state.pagination.index > PAGINATION_VISIBLE_COUNT ?
+                      <React.Fragment>
+                        <Pagination.Item onClick={this.handlePaginationFactory(1)}>{1}</Pagination.Item>
+                        <Pagination.Ellipsis disabled/>
+                      </React.Fragment> :
+                        ""
+                    }
+
+                    {
+                      this.state.pagination.index > 1 ?
+                        <Pagination.Item onClick={this.handlePaginationFactory(this.state.pagination.index - 1)}>{this.state.pagination.index - 1}</Pagination.Item> :
+                        ""
+                    }
+
+                    {
+                      <Pagination.Item active onClick={this.handlePaginationFactory(this.state.pagination.index)}>{this.state.pagination.index}</Pagination.Item>
+                    }
+
+                    {
+                      this.state.pagination.index < this.state.pagination.count ?
+                        <Pagination.Item onClick={this.handlePaginationFactory(this.state.pagination.index + 1)}>{this.state.pagination.index + 1}</Pagination.Item> :
+                        ""
+                    }
+
+                    {
+                      this.state.pagination.index == 1 && this.state.pagination.count > 2 ?
+                        <Pagination.Item onClick={this.handlePaginationFactory(this.state.pagination.index + 2)}>{this.state.pagination.index + 2}</Pagination.Item> :
+                        ""
+                    }
+
+                    {
+                      Math.max(this.state.pagination.index + 1, 3) < this.state.pagination.count ?
+                        <React.Fragment>
+                          <Pagination.Ellipsis disabled/>
+                          <Pagination.Item onClick={this.handlePaginationFactory(this.state.pagination.count)}>{this.state.pagination.count}</Pagination.Item>
+                        </React.Fragment> :
+                        ""
+                    }
+
+                    <Pagination.Next onClick={this.handlePaginationFactory(this.state.pagination.index + 1)} />
+                    <Pagination.Last onClick={this.handlePaginationFactory(this.state.pagination.count)} />
+                  </Pagination>
+
                   <Button id="settings-save-btn"> Save settings</Button>
                 </div>
               </div>
