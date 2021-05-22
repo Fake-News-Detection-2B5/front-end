c29dd4ceda5322b1244f36082e7711bb2a6e0d95
BeterNerfIrelia <danieltudose150@yahoo.com>
2021-05-22 16:39:44 +0300

    [ADD] Filter modal button and update state with form info

--- src/components/utility/FilterModal.jsx
+++ src/components/utility/FilterModal.jsx
@@ -5,7 +5,7 @@ class FilterModal extends Component {
     state = {
         show: false,
         filter: {
-            date: 0,
+            date: 1,
             ascendant: true
         }
     };
@@ -18,8 +18,32 @@ class FilterModal extends Component {
         this.setState({show: true});
     }
 
-    handleFormRadioChange = (event) => {
-        console.log(event.target);        
+    handleFormDateRadioChange = (event) => {
+        //console.log(event.target.id); 
+        let pos = event.target.id.lastIndexOf("-");
+        //console.log(event.target.id.substr(pos+1,1));
+        let id = event.target.id.substr(pos+1,1);
+        let ascendant = this.state.filter.ascendant;
+
+        
+        this.setState({filter: {date: parseInt(id), ascendant: ascendant}});
+        console.log(this.state.filter);
+    }
+
+    handleFromOrderRadioChange = (event) => {
+        let pos = event.target.id.lastIndexOf("-");
+        let id = event.target.id.substr(pos+1,1);
+        let value = id === "1";
+        
+        let date = this.state.filter.date;
+
+        this.setState({filter: {date: date, ascendant: value}});
+        console.log(this.state.filter);
+    }
+
+    handleApplyFilters = async () => {
+        alert('NU AVEM FILTRE');
+        return;
     }
 
     render() {
@@ -34,37 +58,40 @@ class FilterModal extends Component {
                         <ListGroup>
                             <ListGroup.Item>
                                 Date:
-                                <Form>
+                                <Form onChange={this.handleFormDateRadioChange}>
                                     <Form.Check
                                         type="radio"
                                         id="radio-option-date-1"
                                         label="Last Day"
                                         name="radio"
-                                        checked={this.state.filter.date === 0}
+                                        checked={this.state.filter.date === 1}
                                     />
                                     <Form.Check
                                         type="radio"
                                         id="radio-option-date-2"
                                         label="Last Week"
                                         name="radio"
+                                        checked={this.state.filter.date === 2}
                                     />
                                     <Form.Check
                                         type="radio"
                                         id="radio-option-date-3"
                                         label="Last Month"
                                         name="radio"
+                                        checked={this.state.filter.date === 3}
                                     />
                                     <Form.Check
                                         type="radio"
                                         id="radio-option-date-4"
                                         label="Last Year"
                                         name="radio"
+                                        checked={this.state.filter.date === 4}
                                     />
                                 </Form>
                             </ListGroup.Item>
                             <ListGroup.Item>
                                 Order:
-                                <Form>
+                                <Form onChange={this.handleFromOrderRadioChange}>
                                     <Form.Check
                                         type="radio"
                                         id="radio-option-order-1"
@@ -77,10 +104,26 @@ class FilterModal extends Component {
                                         id="radio-option-order-2"
                                         label="Descending"
                                         name="order"
+                                        checked={this.state.filter.ascendant === false}
                                     />
                                 </Form>
                             </ListGroup.Item>
                         </ListGroup>
+
+                        <Modal.Footer>
+                            <Button
+                                variant="secondary"
+                                onClick={this.handleModalClose}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                variant="primary"
+                                onClick={this.handleApplyFilters}
+                            >
+                                Apply filters
+                            </Button>
+                        </Modal.Footer>
                     </Modal.Body>
                 </Modal>
 
